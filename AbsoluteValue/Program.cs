4f0d9b9b1e33715508103d128ac27f60cc6de898
LootingBery <ryborg.benjamin@gmail.com>
2026-01-07 13:11:36 +0100

    Added some notes

--- AbsoluteValue/Program.cs
+++ AbsoluteValue/Program.cs
@@ -4,8 +4,20 @@
     {
         static void Main(string[] args)
         {
+            // Tillader brugeren at ingive en tal
+            Console.WriteLine("Please input you desired number: ");
+            double Number = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("23");
+            // Skal tjekke om tallet er negativt
+            if (Number < 0)
+            {
+            Math.Abs(Number);
+            Console.WriteLine("The absolute value of your number is: " + Math.Abs(Number));
+            }
+            else // Tallet er allerede positivt
+            {
+                Console.WriteLine(Number + "is already an absolute value");
+            }
         }
     }
 }
